24376880dac553ff0ebdab62c8421294fc4e7e4d
ssumai-kr <fhsjdvs@gmail.com>
2025-10-25 17:35:31 +0000

    fix: Test Proxy

--- src/api/apiMember.ts
+++ src/api/apiMember.ts
@@ -1,19 +1,46 @@
+// import useUserStore from "@/store/useUserStore";
+// import axios from "axios";
+// //멤버 api
+
+// function getAccessToken(): string | null {
+//   const { user } = useUserStore.getState();
+//   return user?.accessToken ?? localStorage.getItem("accessToken");
+// }
+// const accessToken = getAccessToken();
+// console.log(accessToken, "ㅌㅌ");
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_MEMBER_SERVER_URL,
+// });
+// api.interceptors.request.use(
+//   (config) => {
+//     if (accessToken) {
+//       config.headers.Authorization = `Bearer ${accessToken}`;
+//     }
+
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   },
+// );
+
+// export default api;
+
 import useUserStore from "@/store/useUserStore";
 import axios from "axios";
 //멤버 api
 
-function getAccessToken(): string | null {
-  const { user } = useUserStore.getState();
-  return user?.accessToken ?? localStorage.getItem("accessToken");
-}
-const accessToken = getAccessToken();
-console.log(accessToken, "ㅌㅌ");
-
 const api = axios.create({
   baseURL: import.meta.env.VITE_MEMBER_SERVER_URL,
 });
+
 api.interceptors.request.use(
   (config) => {
+    const { user } = useUserStore.getState();
+    const accessToken =
+      user?.accessToken ?? localStorage.getItem("accessToken");
+
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
     }
